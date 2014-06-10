9ce5b1208b05c1f03eeabcc25c4f0321496d5001
Bruno Wagner Goncalves <bwagner@thoughtworks.com>
2014-06-10 13:18:37 -0300

    Both loops were connecting things the same way, so I removed one of them

--- lib/flight-control-tower/parser.rb
+++ lib/flight-control-tower/parser.rb
@@ -27,7 +27,7 @@ module FlightControlTower
 
         outbound = content.scan(/\.trigger\((.+?)\)/).map{|e| e[0].split(',').map(&:strip) }
         outbound.each do |args|
-          args.shift if args[0] == 'document' 
+          args.shift if args[0] == 'document'
           results[component_name][:outbound] << args.first
         end
         results[component_name][:inbound].uniq!
@@ -49,18 +49,7 @@ module FlightControlTower
         end
       end
 
-      results.each_pair do |component_name, component_events|
-        component_events[:outbound].each do |event|
-          results.each_pair do |next_component_name, next_component_events|
-            next if component_name == next_component_name
-            if results[next_component_name][:inbound].include?(event)
-              graph << { source: component_name, target: next_component_name, eventName: event }
-            end
-          end
-        end
-      end
       return graph
     end
   end
-end 
- 
+end
